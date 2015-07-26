6edea786edad8dcf5308f5073e84f4650d0b8326
mutsumikodama <mutsumikodama+dev@gmail.com>
2015-07-26 18:52:04 +0900

    Main Program

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    0
+  def self.total_count_for
+    puts("最初に何本購入しますか？")
+    amount=gets.to_i
+    if amount < 0
+      puts("1以上の数字を入れてください")
+    else
+    puts("最初に#{amount}本購入した場合、トータルで#{amount + amount/3}本飲めます。")
+    end
   end
+
+  self.total_count_for()
 end
