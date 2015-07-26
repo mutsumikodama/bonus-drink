0ebdf19a64b3bb32402030de97ae676f5c31e664
mutsumikodama <mutsumikodama+dev@gmail.com>
2015-07-26 19:12:23 +0900

    Limit the number as Integer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,7 +1,7 @@
 class BonusDrink
   def self.total_count_for(amount)
-    return 0 if amount == 0
-    return amount + (amount - 1) / 2
+    return 0 if amount <= 0
+    return amount.to_i + (amount.to_i - 1) / 2
   end
 
   puts "最初に100本購入した場合、トータルで#{BonusDrink.total_count_for 100}本飲めます。"
